99578a31bd53263c40e997e4bf938b5d0a92d42c
W-Jacky <3085449492@qq.com>
2022-09-10 03:31:37 +0800

    Site updated: 2022-09-10 03:31:34

---
+++ js/console.js
@@ -0,0 +1,7 @@
+queueMicrotask((() => {
+    const o = function () {
+    };
+    const e = ["欢迎来到Coder Cool!", "生活明朗, 万物可爱"],
+        t = ["NCC2-036", "调用前置摄像头拍照成功，识别为【小笨蛋】.", "Photo captured: ", "🤪"];
+    setTimeout(o.bind(console, `%c ${t[0]} %c ${t[1]} %c \n${t[2]} %c\n${t[3]}\n`, "color:white; background-color:#4fd953", "", "", 'background:url("https://cdn.cbd.int/anzhiyu-blog@1.1.6/img/post/common/tinggge.gif") no-repeat;font-size:450%')), setTimeout(o.bind(console, "%c WELCOME %c 你好，小笨蛋.", "color:white; background-color:#4f90d9", "")), setTimeout(console.warn.bind(console, "%c ⚡ Powered by W-Jacky %c 你正在访问 W-Jacky 的博客.", "color:white; background-color:#f0ad4e", "")), setTimeout(o.bind(console, "%c W23-12 %c 你已打开控制台.", "color:white; background-color:#4f90d9", "")), setTimeout(console.warn.bind(console, "%c S013-782 %c 你现在正处于监控中.", "color:white; background-color:#d9534f", ""))
+}));
